acaee5398c5268872b6becec8bd28bfbeaa2cec4
jupa005 <antelapad1@gmail.com>
2023-08-13 14:02:53 +0200

    Solved first 7 excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, n) {
+  let word = "";
+  if (n < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < n; i++) {
+    word += string;
+  }
+  return word;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  if (
+    typeof firstNumber !== "number" ||
+    typeof secondNumber !== "number" ||
+    firstNumber < 0 ||
+    secondNumber < 0
+  ) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (firstNumber < secondNumber) {
+    while (firstNumber <= secondNumber) {
+      sum += firstNumber;
+      firstNumber++;
+    }
+  } else if (firstNumber > secondNumber) {
+    while (secondNumber <= firstNumber) {
+      sum += secondNumber;
+      secondNumber++;
+    }
+  } else {
+    sum = firstNumber;
+  }
+  return sum;
 };
 
 // Do not edit below this line
